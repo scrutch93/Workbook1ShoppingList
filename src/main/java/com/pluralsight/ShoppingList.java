ec02671b7d1280a0a639547006c142d136248ae4
scrutch93 <stevccru@icloud.com>
2023-10-04 11:32:51 -0400

    added breaks and numbering to list

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -3,6 +3,6 @@ package com.pluralsight;
 public class ShoppingList {
 
     public static void main (String[] args){
-        System.out.println("eggs, cheese, milk, bread, butter, rice, cookies, fruit, soda, chips");
+        System.out.println("ShoppingList: \n1.eggs, \n2.cheese, \n3.milk, \n4.bread, \n5.butter, \n6.rice,\n7.cookies, \n8.fruit, \n9.soda, \n10.chips");
     }
 }
